a2c887d43e382e1b43a9793fe48d69005348e494
任海 <hai.ren@renren-inc.com>
2014-03-07 00:47:05 +0800

    适配ios6 tabbar 样式

--- KelpGang/KGViewController.m
+++ KelpGang/KGViewController.m
@@ -20,10 +20,25 @@
 {
     [super viewDidLoad];
     self.delegate = self;
-//    UITabBar *tabbar = [self tabBar];
-//    UITabBarItem * tabbarItem = [[tabbar items] objectAtIndex:0];
-//    UIImage *selectedImage = [UIImage imageNamed:kTabbarItemSelectedImage];
-//    [tabbarItem setSelectedImage:selectedImage];
+    if (![self isHigherIOS7]) {
+        UIImage *tabbarImage = [UIImage imageNamed:@"tab_bar.png"];
+        UIColor *imageTintColor = [UIColor colorWithRed:33.0 / 255 green:185.0 / 255 blue:162.0 / 255 alpha:1.0];
+        UIColor *tintColor = [UIColor colorWithRed:233.0 / 255 green:243.0 / 255 blue:243.0 / 255 alpha:1.0];
+        [[UITabBar appearance] setBackgroundImage:tabbarImage];
+//        [[UITabBar appearance] setSelectedImageTintColor: imageTintColor];
+        [[UITabBar appearance] setTintColor:tintColor];
+        [[self.tabBar.items objectAtIndex:0] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:imageTintColor,UITextAttributeTextColor,nil] forState:UIControlStateNormal];
+        UIImage *selectedImage1 = [UIImage imageNamed:@"search_someone_active.png"];
+        UIImage *selectedImage2 = [UIImage imageNamed:@"search_active.png"];
+        UIImage *selectedImage3 = [UIImage imageNamed:@"write_active.png"];
+        UIImage *selectedImage4 = [UIImage imageNamed:@"me_active.png"];
+        UIImage *selectedImage5 = [UIImage imageNamed:@"more_active.png"];
+        NSArray *selectedImages = @[selectedImage1, selectedImage2,selectedImage3,selectedImage4,selectedImage5];
+        NSInteger index = 0;
+        for (UITabBarItem *item in self.tabBar.items) {
+            [item setSelectedImage:[selectedImages objectAtIndex: index ++]];
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -33,11 +48,27 @@
 }
 
 - (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
-//    UITabBar *tabbar = [tabBarController tabBar];
-//    UITabBarItem * tabbarItem = [tabbar selectedItem];
-//    UIImage *selectedImage = [UIImage imageNamed:kTabbarItemSelectedImage];
-//    [tabbarItem setSelectedImage:selectedImage];
+    if (![self isHigherIOS7]) {
+        [self.selectedViewController.tabBarItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor colorWithRed:33.0 / 255 green:185.0 / 255 blue:162.0 / 255 alpha:1.0],UITextAttributeTextColor,nil] forState:UIControlStateNormal];
+        NSInteger index = 0;
+        for (UITabBarItem *item in self.tabBar.items) {
+            if (self.selectedIndex != index ++) {
+                [item setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor lightGrayColor],UITextAttributeTextColor,nil] forState:UIControlStateNormal];
+            }
+        }
+    }
 
+}
+
+- (BOOL)isHigherIOS7 {
+    NSString *requestSysVer = @"7.0";
+    NSString *currentSysVer = [[UIDevice currentDevice] systemVersion];
+
+    if ([currentSysVer compare:requestSysVer options:NSNumericSearch] == NSOrderedAscending) {
+        return NO;
+    }
+
+    return YES;
 }
 
 @end
