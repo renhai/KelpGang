ed80a3a4a54de23178c620b118a192111ad0d110
任海 <hai.ren@renren-inc.com>
2014-03-17 12:37:55 +0800

    添加更多页面的ViewController

---
+++ KelpGang/ViewControllers/KGMoreInfoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KGMoreInfoTableViewController.h
+//  KelpGang
+//
+//  Created by Andy on 14-3-17.
+//  Copyright (c) 2014年 renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGMoreInfoTableViewController : UITableViewController
+
+@end
