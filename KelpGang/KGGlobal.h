11788c5bdf2e152bf9a86adf3bb07097ad2bb24d
任海 <hai.ren@renren-inc.com>
2014-04-18 14:20:28 +0800

    add macro

--- KelpGang/KGGlobal.h
+++ KelpGang/KGGlobal.h
@@ -6,46 +6,6 @@
 //  Copyright (c) 2014年 renren. All rights reserved.
 //
 
-#define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
-#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
-
-/*
- *屏幕宽度
- */
-#define SCREEN_WIDTH ([[UIScreen mainScreen]bounds].size.width)
-
-/*
- *屏幕高度
- */
-#define SCREEN_HEIGHT ([[UIScreen mainScreen]bounds].size.height)
-
-/*
- * iPhone 屏幕尺寸
- */
-#define SCREEN_SIZE (CGSizeMake(SCREEN_WIDTH, SCREEN_HEIGHT - PHONE_STATUSBAR_HEIGHT))
-
-/*
- * iPhone statusbar 高度
- */
-#define STATUSBAR_HEIGHT 20
-
-/*
- * iPhone 默认导航条高度
- */
-#define NAVIGATIONBAR_HEIGHT 44.0f
-#define NAVIGATIONBAR_IOS7_HEIGHT 64.0f
-// tabBar高度
-#define TABBAR_HEIGHT 50.0f
-#define ENGISH_KEYBOARD_HEIGHT 216.0
-
-
-//判断是否为iPhone5
-#define iPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
-//判断是否为Retina
-#define isRetina ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] && [[UIScreen mainScreen] scale] == 2.0)
-
-#define LINE_HEIGHT (isRetina ? 0.5 : 1.0)
-
 #define kPublishTask @"kPublishTask"
 #define kXMPPNewMsgNotifaction @"xmppNewMsgNotifaction"
 #define kChatHostName @"10.2.45.77"
