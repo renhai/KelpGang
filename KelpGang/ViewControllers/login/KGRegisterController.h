e68be76e756a72800852fe5344479629a3155990
Andy <andy@Andy-MacBook-Pro.local>
2014-06-27 18:35:17 +0800

    add login view controller

---
+++ KelpGang/ViewControllers/login/KGRegisterController.h
@@ -0,0 +1,13 @@
+//
+//  KGRegisterController.h
+//  KelpGang
+//
+//  Created by Andy on 14-6-19.
+//  Copyright (c) 2014年 renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGRegisterController : UITableViewController
+
+@end
